53630003b645119b2fbb425d47306748b8dca5f4
sashuk <shumsan1011@gmail.com>
2018-12-20 13:23:07 +0000

    New touch backend was added to react-dnd

--- browser/components/withDragDropContext.js
+++ browser/components/withDragDropContext.js
@@ -1,4 +1,7 @@
 import {DragDropContext} from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
+var MultiBackend = require('react-dnd-multi-backend').default;
+var HTML5toTouch = require('react-dnd-multi-backend/lib/HTML5toTouch').default; // or any other pipeline
 
-export default DragDropContext(HTML5Backend);
+
+export default DragDropContext(MultiBackend(HTML5toTouch));
