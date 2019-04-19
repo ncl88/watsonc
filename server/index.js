25d37404d1e5c7c9a5f98a6c187a65ae9679e5de
sashuk <shumsan1011@gmail.com>
2019-04-19 13:13:24 +0000

    Querying the backend

--- server/index.js
+++ server/index.js
@@ -5,21 +5,26 @@
  */
 
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 var moment = require('moment');
+var config = require('./../../../config/config');
 
 moment.locale("da_DK");
 
-router.post('/api/extension/watsonc', function (request, response) {
+router.post('/api/extension/watsonc', function (req, res) {
+    if (!config.gc2.host) throw new Error(`GC2 host has to be specified`);
 
-    console.log(request.body);
+    let table = `chemicals.boreholes_time_series_with_chemicals`;
+    let url = config.gc2.host + `/api/sql/jupiter?base64=true&srs=4326&lifetime=0&client_encoding=UTF8&key=null`;
+    url += `&q=` + Buffer.from(`SELECT * FROM ${table} WHERE ST_Intersects(ST_Transform(ST_geomfromtext('${JSON.stringify(req.body)}', 4326), 25832), the_geom)`).toString('base64');
 
-    /*
-        Re-use code from SQL query API
-        Request intersecting boreholes from a specific table
-    */
+    request.get(url, function (err, localRes, body) {
 
-    response.send({status: `success`});
+        console.log(err, body);
+
+        res.send({status: `success`});
+    }); 
 });
 
 module.exports = router;
