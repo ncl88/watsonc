c3fc97a07d85cee1bbbc7b303a5e559ed751edb7
sashuk <shumsan1011@gmail.com>
2019-06-28 10:44:43 +0000

    Only intersection script is now public, the profile script resides in the different repository

--- config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    intersectionsScriptPath: '/usr/share/nginx/html/extensions/watsonc/scripts/intersectiontool.py',
-    profileScriptPath: '/usr/share/nginx/html/extensions/watsonc/scripts/profiletool.py',
-};
