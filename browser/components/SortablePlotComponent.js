521e13ecce0f5564e3e94413659235e7289bc692
sashuk <shumsan1011@gmail.com>
2019-07-18 06:03:15 +0000

    Correct display of plots and profiles

--- browser/components/SortablePlotComponent.js
+++ browser/components/SortablePlotComponent.js
@@ -8,7 +8,7 @@ import PlotComponent from './PlotComponent';
  * Wrapper for making a Plot component sortable inside of Plots grid
  */
 const SortablePlotComponent = (props) => {
-    return (<li className={props.containerClass ? props.containerClass : `list-group-item col-sm-12 col-md-12 col-lg-6`}>
+    return (<li className={props.containerClass ? props.containerClass : `list-group-item col-sm-12 col-md-12 col-lg-6`} style={{minHeight: `520px`}}>
         <div>
             <PlotComponent onDelete={(id) => { props.handleDelete(id)}} plotMeta={props.meta}/>
         </div>
